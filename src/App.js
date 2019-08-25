41f84ed114a3fd10f766ab3b0274cd49454940dc
Luis Chodiman <lechodiman@uc.cl>
2019-08-25 18:27:06 -0400

    Add firebase sdk config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+import firebase from "./firebase";
 
-function App() {
+const db = firebase.database();
+
+const App = props => {
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState({});
+  const [joined, setJoined] = useState(false);
+
+  const chatRoom = db
+    .ref()
+    .child("chatrooms")
+    .child("global");
+
+  useEffect(() => {
+    const handleNewMessages = snap => {
+      console.log(snap.val());
+      if (snap.val()) {
+        setMessages(snap.val());
+      }
+    };
+
+    chatRoom.on("value", handleNewMessages);
+
+    return () => {
+      chatRoom.off("value", handleNewMessages);
+    };
+  }, []);
+
+  const handleNameChange = e => setNickname(e.target.value);
+  const handleEmailChange = e => setEmail(e.target.value);
+
+  const handleClick = e => {
+    db.ref()
+      .child("nicknames")
+      .push({
+        nickname,
+        email
+      });
+    setJoined(true);
+  };
+
+  const handleMessageChange = e => setMessage(e.target.value);
+
+  // Not sure why this is here
+  const handleKeyDown = e => {
+    if (e.key === "Enter") {
+      chatRoom.push({
+        sender: nickname,
+        message
+      });
+      setMessage("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!joined ? (
+        <div className="joinForm">
+          <input
+            type="text"
+            placeholder="Nickname"
+            value={nickname}
+            onChange={handleNameChange}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <br />
+          <button onClick={handleClick}>Join</button>
+        </div>
+      ) : (
+        <div className="chat">
+          <div className="messages">
+            {Object.keys(messages).map(key => {
+              if (messages[key]["sender"] === nickname) {
+                return (
+                  <div className="message" key={key}>
+                    <span id="me">{messages[key]["sender"]} :</span>
+                    <br />
+                    {messages[key]["message"]}
+                  </div>
+                );
+              }
+
+              return (
+                <div className="message" key={key}>
+                  <span id="sender">{messages[key]["sender"]} :</span>
+                  <br />
+                  {messages[key]["message"]}
+                </div>
+              );
+            })}
+          </div>
+          <input
+            type="text"
+            placeholder="msg"
+            onChange={handleMessageChange}
+            value={message}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      )}
     </div>
   );
-}
+};
+
+App.propTypes = {};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyBtum51srsX4BZVhzsiwyZj0yXBEMe2W8E",
+  authDomain: "chat-for-dogs.firebaseapp.com",
+  databaseURL: "https://chat-for-dogs.firebaseio.com",
+  projectId: "chat-for-dogs",
+  storageBucket: "",
+  messagingSenderId: "625853169616",
+  appId: "1:625853169616:web:3448447c858ece74"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export default firebase;
