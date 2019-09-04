61a5c185a96b4b14075b311747f8c02f920f371b
Luis Chodiman <lechodiman@uc.cl>
2019-09-04 10:28:26 -0400

    Rename server.py to app.py

--- app.py
+++ app.py
@@ -6,8 +6,3 @@ app = Flask(__name__, static_folder="build/static", template_folder="build")
 @app.route("/")
 def hello():
     return render_template('index.html')
-
-
-print('Starting Flask!')
-app.debug = True
-app.run(host='0.0.0.0')
