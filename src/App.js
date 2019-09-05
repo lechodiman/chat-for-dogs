d9f304298f0bbb8b08fca8638b612d9bde223e7d
Luis Chodiman <lechodiman@uc.cl>
2019-09-05 13:29:00 -0400

    Add link to history

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ChatRoom from "./components/ChatRoom";
 import History from "./components/History";
+import { Link } from "react-router-dom";
 
 const App = props => {
   return (
     <Router>
+      <nav>
+        <Link to="/history">History </Link>
+        <Link to="/">ChatRoom</Link>
+      </nav>
       <Switch>
         <Route exact path="/" component={ChatRoom}></Route>
         <Route exact path="/history" component={History}></Route>
